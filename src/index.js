4dd7c442a906bb0d92d5433fc827405a54a217a0
rakesh-donga <136368164+rakesh-donga@users.noreply.github.com>
2025-12-04 23:38:20 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import 'bpk-stylesheets/base.css';
+import 'bpk-stylesheets/font.css';
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
